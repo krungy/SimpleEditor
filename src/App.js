bd8203bd41325cd127991df691440e102c079ea4
krungy <furler711@gmail.com>
2022-02-27 18:57:59 +0900

    ✨ feat: App에서 LocalStorage 값 가져오기 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import styles from './assets/css/App.module.css';
 import MainContainer from './modules/MainContainer/MainContainer.js';
 import SideMenu from './modules/SideMenu/SideMenu.js';
+import { getItem } from './storage';
 
 export default function App({ $target }) {
   $target.className = styles.app;
 
+  this.state = [];
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+  };
+
+  const documents = getItem('documents', []);
+  console.log(documents);
+  this.setState(documents);
+
   const sidemenu = new SideMenu({
     $target,
+    initialState: [],
   });
 
   const mainContainer = new MainContainer({
