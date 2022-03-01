2a99fa9673b84b4ab8a92775e88305dba18959c3
krungy <furler711@gmail.com>
2022-03-01 20:00:33 +0900

    ♻️ refactor: App에서 LocalStorage 값 가져오기 수정

--- src/App.js
+++ src/App.js
@@ -6,22 +6,39 @@ import { getItem } from './storage';
 export default function App({ $target }) {
   $target.className = styles.app;
 
-  this.state = [];
-
-  this.setState = (nextState) => {
-    this.state = nextState;
+  this.state = {
+    isLoading: false,
+    documents: [],
   };
 
-  const documents = getItem('documents', []);
-  console.log(documents);
-  this.setState(documents);
-
-  const sidemenu = new SideMenu({
+  new SideMenu({
     $target,
-    initialState: [],
+    initialState: this.state.documents,
+    onDocumentAdd: () => {},
   });
 
   const mainContainer = new MainContainer({
     $target,
   });
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+  };
+
+  const fetchRequests = () => {
+    this.setState({
+      ...this.state,
+      isLoading: true,
+    });
+    const documents = getItem('documents', []);
+
+    this.setState({
+      ...this.state,
+      documents,
+      isLoading: false,
+    });
+  };
+
+  fetchRequests();
+  console.log(this.state);
 }
